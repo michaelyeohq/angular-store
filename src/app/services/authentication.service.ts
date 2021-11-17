b88e54bc7698233ff3cef6c6c474e021b9f2bc8f
michael.yeo <michael.yeo@temus.com>
2021-11-17 19:08:08 +0800

    Added auth services

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor() { }
+}
